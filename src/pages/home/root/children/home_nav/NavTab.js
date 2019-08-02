0cfb3d031b44c51acbc87f885cc8ee50340047a2
520LaiQinghua <2655976664@qq.com>
2019-08-02 09:07:15 +0800

    home=>nav=>nav_son=>navTab/context OK

---
+++ src/pages/home/root/children/home_nav/NavTab.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import './nav.css'
+ export default class NavTab extends Component {
+    state={
+      selectedIndex:0
+    }
+  
+  render() {
+    let navs = [
+      { id:2221,title: '职业提升' },
+      { id:2222,title: '编程与开发' },
+      { id:2223,title: 'AI/数据科学' },
+      { id:2224,title: '产品与运营' },
+      { id:2225,title: '设计创意' },
+      { id:2226,title: '电商运营' },
+      { id:2227,title: '语音学习' },
+      { id:2228,title: '职业考试' },
+      { id:2229,title: '生活兴趣' },
+    ]
+    return (
+      <div id="nav">
+        <ul>
+      {
+       navs.map((item,index)=>(
+         <li key={item.id}
+          onClick={()=>this.selectedIndex(index)}
+          className={this.state.selectedIndex === index? 'nav_active':''}>
+         {item.title}
+         </li>
+       ))
+      }
+      </ul>
+      </div>
+    );
+  }
+  selectedIndex=(index)=>{
+    this.setState({selectedIndex:index})
+    this.props.receivedIndex(index)
+  }
+}
+
--- src/pages/home/root/children/home_nav/nav_son.js
+++ src/pages/home/root/children/home_nav/nav_son.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import AppScroll from '../../../../../components/app-scroll/AppScroll'
+import './nav.css'
 
+import Header from '../../../../../components/app-header/index'
+import NavTab from './NavTab'
 class nav_son extends Component {
+    state={ 
+        // currentIndex:0,
+        // id:this.props.match.params,
+        receivedIndex:0
+    }
+
     render() {
+        let Return =<span className="iconfont icon-changyongicon-1" onClick={this.handleReturn}></span>
+        // let { currentIndex} = this.state
         return (
             <div>
-                
+               <Header props={this.props} Return={Return}/> 
+               <NavTab receivedIndex={this.receivedIndex} props={this.props}/>
+                <AppScroll className="scrollContent">
+                 <div>
+                     <section>
+                      <ul>
+                          <li>个人理财</li>
+                          <li>个人理财</li>
+                          <li>个人理财</li>
+                          <li>个人理财</li>
+                          <li>个人理财</li>
+                          <li>个人理财</li>
+                          <li>个人理财</li>
+                      </ul>
+                     </section>
+                     <section>
+
+                     </section>
+                 </div>
+                </AppScroll>
             </div>
         );
     }
+    handleReturn=()=>{
+        this.props.history.goBack();
+        }
 }
 
 export default nav_son;
